a3ce4bb8eea00d7a23f91b5be412168c988f5fff
王子晨 <wangzichen@SherlockMac.local>
2017-07-30 21:16:04 +0800

    04-shop头部视图的实现

--- MeiTuan/Classes/Shop/MTShopController.m
+++ MeiTuan/Classes/Shop/MTShopController.m
@@ -9,7 +9,15 @@
 #import "MTShopController.h"
 #import "MTFoodDetailController.h"
 
+//头部视图的最大高度
+#define KShopHeaderViewMaxHeight   180
+//头部视图的最小高度
+#define KShopHeaderViewMinHeight   64
+
 @interface MTShopController ()
+
+//头部视图
+@property (nonatomic, weak) UIView *shopHeaderView;
 
 @end
 
@@ -17,6 +25,10 @@
 
 - (void)viewDidLoad
 {
+    
+    //此方法优先于导航条加载
+    [self setupUI];
+    
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor orangeColor];
@@ -27,11 +39,58 @@
     
 }
 
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+//界面搭建
+- (void)setupUI
 {
-    MTFoodDetailController *foodDetailVC = [[MTFoodDetailController alloc] init];
+    //创建头部视图
+    UIView *shopHeaderView = [[UIView alloc] init];
     
-    [self.navigationController pushViewController:foodDetailVC animated:YES];
+    shopHeaderView.backgroundColor = [UIColor purpleColor];
+    
+    [self.view addSubview:shopHeaderView];
+    
+    //设置约束
+    [shopHeaderView mas_makeConstraints:^(MASConstraintMaker *make)
+     {
+        make.left.top.right.offset(0);
+         make.height.offset(180);
+     }];
+    
+    //添加平移手势
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panGesture:)];
+    
+    [self.view addGestureRecognizer:pan];
+    
+    //给全局属性赋值
+    _shopHeaderView = shopHeaderView;
+    
+
 }
+
+
+//平移手势
+- (void)panGesture:(UIPanGestureRecognizer *)pan
+{
+    //获取平移距离
+    CGPoint p = [pan translationInView:pan.view];
+    
+    //更新约束
+    [_shopHeaderView mas_updateConstraints:^(MASConstraintMaker *make)
+    {
+        make.height.offset(p.y + _shopHeaderView.bounds.size.height);
+    }];
+    
+    //恢复到初始值
+    [pan setTranslation:CGPointZero inView:pan.view];
+    
+    
+}
+
+//- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+//{
+//    MTFoodDetailController *foodDetailVC = [[MTFoodDetailController alloc] init];
+//    
+//    [self.navigationController pushViewController:foodDetailVC animated:YES];
+//}
 
 @end
