bb979d30dc0d866fafdc134d5e6ffed0458af583
王子晨 <wangzichen@sherlockmac.local>
2017-07-30 20:48:48 +0800

    03-导航条左边返回按钮的实现

--- MeiTuan/Classes/MTBaseController.m
+++ MeiTuan/Classes/MTBaseController.m
@@ -14,30 +14,47 @@
 
 @implementation MTBaseController
 
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    
+    if (self) {
+        //每一个继承该类的控制器都创建一个导航条
+        UINavigationBar *navBar = [[UINavigationBar alloc] init];
+
+        
+        //给导航条添加item
+        UINavigationItem *navItem = [[UINavigationItem alloc] init];
+        
+        [navBar setItems:@[navItem]];
+        
+        //给全局属性赋值
+        _navBar = navBar;
+        _navItem = navItem;
+    }
+    
+    return self;
+    
+    
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
-    //每一个继承该类的控制器都创建一个导航条
-    UINavigationBar *navBar = [[UINavigationBar alloc] init];
+    
     //添加到主视图上
-    [self.view addSubview:navBar];
+    [self.view addSubview:_navBar];
     
     //设置导航条的约束
-    [navBar mas_makeConstraints:^(MASConstraintMaker *make)
+    [_navBar mas_makeConstraints:^(MASConstraintMaker *make)
      {
          make.left.top.right.offset(0);
          make.height.offset(64);
      }];
     
-    //给导航条添加item
-    UINavigationItem *navItem = [[UINavigationItem alloc] init];
     
-    [navBar setItems:@[navItem]];
-    
-    //给全局属性赋值
-    _navBar = navBar;
-    _navItem = navItem;
 }
 
 //内存警告处理
--- MeiTuan/Classes/MTNavigationController.m
+++ MeiTuan/Classes/MTNavigationController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MTNavigationController.h"
+#import "MTBaseController.h"
 
 @interface MTNavigationController ()
 
@@ -20,5 +21,27 @@
     
     //隐藏导航条
     self.navigationBar.hidden = YES;
+}
+
+- (void)pushViewController:(MTBaseController *)viewController animated:(BOOL)animated
+{
+    [super pushViewController:viewController animated:animated];
+    
+    //只给除导航控制器之外的其余子控制器添加左边返回按钮
+    if (self.childViewControllers.count > 1)
+    {
+        //给导航条设置左边按钮
+        viewController.navItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"btn_backItem"] style:UIBarButtonItemStylePlain target:self action:@selector(backBtnClick)];
+    }
+    
+    
+}
+
+
+- (void)backBtnClick
+{
+    //点击按钮返回
+    [self popViewControllerAnimated:YES];
+
 }
 @end
